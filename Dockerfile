3264edb2118fa2fa779e967ea8aae2bb7d2ab567
xxstop <xxstop@qq.com>
2016-06-15 23:40:14 +0800

    .

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,5 @@ RUN apt-get -y upgrade
 RUN apt-get install -y supervisor cron
 
 RUN sed -i s/required/sufficient/g /etc/pam.d/cron
-RUN crontab /etc/crontab
 
 CMD ["/usr/bin/supervisord"]
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 CMD="docker run -d --name cron --privileged 
-	-v $BSDIR/etc/crontab:/etc/crontab
+	-v $BSDIR/etc/crontab:/var/spool/cron/crontabs/root
 	-v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
 	-v $BSDIR/data:/data
 	xxstop/cron"
