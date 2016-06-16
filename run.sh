cb0cdd9c950625ee8da35e96458619162abab74d
xxstop <xxstop@qq.com>
2016-06-17 07:46:50 +0800

    .

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 CMD="docker run -d --name phddns --privileged 
-	-v $BSDIR/bin/phddns_run:/usr/local/bin/phddns_run
 	-v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
 	-p 16062:16062/tcp
 	xxstop/phddns"
