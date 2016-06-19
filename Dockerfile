324ad400960d363a7474a5a46fc9618290929959
xxstop <xxstop@qq.com>
2016-06-19 15:38:32 +0800

    .

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,7 @@
-FROM resin/rpi-raspbian:latest
+FROM xxstop/base
 
 MAINTAINER xxstop <xxstop@qq.com>
 
-RUN apt-get update
-RUN apt-get -y upgrade
 RUN apt-get -y install dnsmasq
 
 EXPOSE 53 53/udp
-ENTRYPOINT ["dnsmasq", "-k"]
--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 CMD="docker run -d --name dnsmasq --privileged 
-	-v $BSDIR/etc/dnsmasq.conf:/etc/dnsmasq.conf
 	-v $BSDIR/etc/associate.d:/etc/associate.d
 	-v $BSDIR/etc/dnsmasq.d:/etc/dnsmasq.d 
+	-v $BSDIR/etc/dnsmasq.conf:/etc/dnsmasq.conf
+	-v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
 	-p 53:53/tcp -p 53:53/udp
 	xxstop/dnsmasq"
 
