1d746e41cea150b4122ffc097de3e13b7f200c53
xxstop <xxstop@qq.com>
2016-06-19 09:52:17 +0800

    .

--- run.sh
+++ run.sh
@@ -3,8 +3,7 @@ BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 CMD="docker run -d --name dnsmasq --privileged 
 	-v $BSDIR/etc/dnsmasq.conf:/etc/dnsmasq.conf
-	-v $BSDIR/etc/hosts.dnsmasq:/etc/hosts.dnsmasq
-	-v $BSDIR/etc/resolv.dnsmasq.conf:/etc/resolv.dnsmasq.conf
+	-v $BSDIR/etc/associate.d:/etc/associate.d
 	-v $BSDIR/etc/dnsmasq.d:/etc/dnsmasq.d 
 	-p 53:53/tcp -p 53:53/udp
 	xxstop/dnsmasq"
