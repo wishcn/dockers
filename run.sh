84e5cdffd0801ddf091556a2c0be668c0af29dae
xxstop <xxstop@qq.com>
2016-06-21 21:28:32 +0800

    .

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 CMD="docker run -d --name mariadb --privileged
+	-v $BSDIR/scripts:/usr/local/scripts
+	-v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
         xxstop/mariadb"
 
 echo $CMD
