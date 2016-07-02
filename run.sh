ed771652006c99e6a5f349f04dda576a57b2a2fb
xxstop <xxstop@qq.com>
2016-07-02 23:55:25 +0800

    Add phpfpm

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 CMD="docker run -d --name nginxphp --privileged 
+	-v $BSDIR/data:/data
+	-v $BSDIR/etc/nginx/sites-available:/etc/nginx/sites-available
+	-v $BSDIR/scripts:/usr/local/scripts
 	-v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
 	-p 80:80/tcp -p 80:80/udp
 	-p 8000:8000/tcp -p 8000:8000/udp
