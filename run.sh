19db85fc49873f4822bbf1a3bcb781f832efd740
Mr.Qiu <im.xxstop@gmail.com>
2016-08-31 13:55:55 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 CMD="docker run -d --name nginxphp --privileged 
-	--link=mariadb:mysql
-	-v $BSDIR/data:/data
-	-v $BSDIR/etc/nginx/sites-available:/etc/nginx/sites-available
-	-v $BSDIR/scripts:/usr/local/scripts
-	-v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
-	-p 80:80/tcp -p 80:80/udp
-	-p 8000:8000/tcp -p 8000:8000/udp
-	xxstop/nginxphp"
+    --link=mariadb:mysql
+    -v $BSDIR/data:/data
+    -v $BSDIR/etc/nginx/sites-available:/etc/nginx/sites-available
+    -v $BSDIR/scripts:/usr/local/scripts
+    -v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
+    -p 80:80/tcp -p 80:80/udp
+    -p 8000:8000/tcp -p 8000:8000/udp
+    xxstop/nginxphp"
 
 echo $CMD
 $CMD
