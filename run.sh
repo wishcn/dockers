ed80e99db2d80261888e57d0e21e13c21082da81
Mr.Qiu <im.xxstop@gmail.com>
2016-08-31 13:52:37 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 CMD="docker run -d --name mariadb --privileged
-	-v $BSDIR/scripts:/usr/local/scripts
-	-v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
-        xxstop/mariadb"
+     -v $BSDIR/scripts:/usr/local/scripts
+     -v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
+     xxstop/mariadb"
 
 echo $CMD
 $CMD
