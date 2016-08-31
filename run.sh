7f0508dafab90b99d36e21112c200853b775b991
Mr.Qiu <im.xxstop@gmail.com>
2016-08-31 13:51:55 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 CMD="docker run -d --name crond --privileged
-	-v $BSDIR/data:/data
-	-v $BSDIR/scripts:/usr/local/scripts
-        -v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
-        xxstop/crond"
+     -v $BSDIR/data:/data
+     -v $BSDIR/scripts:/usr/local/scripts
+     -v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
+     xxstop/crond"
 
 echo $CMD
 $CMD
