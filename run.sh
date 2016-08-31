3579d6babc099eba167fe79f6cf7fa5690d74172
Mr.Qiu <im.xxstop@gmail.com>
2016-08-31 13:58:18 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 CMD="docker run -d --name phddns --privileged 
-	-v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
-	-p 16062:16062/tcp
-	xxstop/phddns"
+    -v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
+    -p 16062:16062/tcp
+    xxstop/phddns"
 
 echo $CMD
 $CMD
