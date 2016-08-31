31e06c72b40dcb87d79c40d9b7547b170e6ffd92
Mr.Qiu <im.xxstop@gmail.com>
2016-08-31 13:51:18 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-CMD="docker run -d --name mysql --privileged -v $BSDIR/bakdata:/usr/share/bakdata -v $BSDIR/script:/usr/share/script -p 3306:3306/udp -p 3306:3306/tcp xxstop/mysql"
+CMD="docker run -d --name mysql --privileged 
+    -v $BSDIR/bakdata:/usr/share/bakdata 
+    -v $BSDIR/script:/usr/share/script 
+    -p 3306:3306/udp -p 3306:3306/tcp xxstop/mysql"
 
 echo $CMD
 $CMD
