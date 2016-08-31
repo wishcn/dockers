fe7d38cdfe28f440fc7d0ad3248c131560bff4da
qiuwenxi <qiuwenxi@zbj.com>
2016-08-31 13:49:15 +0800

    Fix tab to space

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 CMD="docker run -d --name aria2 --privileged 
     -v $BSDIR/data/down:/data/down
     -v $BSDIR/data/etc:/data/etc
-	-v $BSDIR/etc/aria2.conf:/etc/aria2.conf
-	-v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
-	-p 6800:6800/tcp -p 6800:6800/udp
-	-p 81:81/tcp -p 81:81/udp
-	xxstop/aria2"
+    -v $BSDIR/etc/aria2.conf:/etc/aria2.conf
+    -v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
+    -p 6800:6800/tcp -p 6800:6800/udp
+    -p 81:81/tcp -p 81:81/udp
+    xxstop/aria2"
 
 echo $CMD
 $CMD
