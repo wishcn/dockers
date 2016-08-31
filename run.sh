f77ceb2676198a5bbc8593455b3efb0f28a5efdd
Mr.Qiu <im.xxstop@gmail.com>
2016-08-31 13:52:21 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 CMD="docker run -d --name dnsmasq --privileged 
-	-v $BSDIR/etc/associate.d:/etc/associate.d
-	-v $BSDIR/etc/dnsmasq.d:/etc/dnsmasq.d 
-	-v $BSDIR/etc/dnsmasq.conf:/etc/dnsmasq.conf
-	-v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
-	-p 53:53/tcp -p 53:53/udp
-	xxstop/dnsmasq"
+     -v $BSDIR/etc/associate.d:/etc/associate.d
+     -v $BSDIR/etc/dnsmasq.d:/etc/dnsmasq.d 
+     -v $BSDIR/etc/dnsmasq.conf:/etc/dnsmasq.conf
+     -v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
+     -p 53:53/tcp -p 53:53/udp
+     xxstop/dnsmasq"
 
 echo $CMD
 $CMD
