278ac8d48d06af3acbd5c9d9c8aabb6817f73754
xxstop <xxstop@qq.com>
2016-08-31 21:14:50 +0800

    Add other config

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ BSDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 CMD="docker run -d --name cow --privileged 
     -v $BSDIR/.cow/rc:/root/.cow/rc
+    -v $BSDIR/etc/cow:/etc/cow
 	-v $BSDIR/supervisord.conf:/etc/supervisor/conf.d/supervisord.conf
 	-p 7777:7777/tcp -p 7777:7777/udp
 	xxstop/cow"
